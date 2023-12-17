4ae70e3691e395dd890e570b55b99a06e45dd0ef
Alexander Carlström <alexander.carlstrom@tietoevry.com>
2023-12-17 19:27:31 +0100

    Continue day 12

--- aoc2023/day12/day12.py
+++ aoc2023/day12/day12.py
@@ -1,4 +1,4 @@
-import utils
+import sys
 import re
 
 
@@ -16,7 +16,8 @@ def generate_variants(input_line):
 
 
 keys = {}
-def gen_variants(dots, groups, i, gi, gl):
+
+
 
 
 
@@ -36,27 +37,34 @@ def count_groups(input_row):
     return spring_groups
 
 
-D = utils.parse_single_string('ex.txt')
-p1 = 0
-p2 = 0
-for line in D:
-    row, groups = line.split(' ')
-    groups = [int(g) for g in groups.split(',')]
+# MAIN PARTS
 
-    variants = generate_variants(row)
-    base_possible = len(list(filter(lambda v: count_groups(v) == groups, variants)))
-    possible = 1
-
-    if all(char in '#?' for char in row[len(row) - groups[-1]:]):
-        possible = base_possible
-    else:
-        possible = len(list(filter(lambda v: count_groups(v) == groups, generate_variants('?' + row))))
-
-    print(base_possible * pow(possible, 4))
-
-    p1 += base_possible
-    p2 += base_possible * pow(possible, 4)
+D = open(sys.argv[1]).read().strip().split('\n')
 
 
-print('Part 1:', p1)
-print('Part 2:', p2)
+def solve_p1():
+    count = 0
+    for line in D:
+        dots, groups = line.split(' ')
+        groups = [int(g) for g in groups.split(',')]
+        variants = generate_variants(dots)
+        count += len(list(filter(lambda v: count_groups(v) == groups, variants)))
+
+    print('Part 1:', count)
+
+
+def solve_p2():
+    count = 0
+    for line in D:
+        dots, groups = line.split(' ')
+        groups = [int(g) for g in groups.split(',')]*5
+        dots = '?'.join([dots]*5)
+        print('dots', dots)
+        print('groups', groups)
+        # variants = generate_variants(dots)
+        # count += len(list(filter(lambda v: count_groups(v) == groups, variants)))
+
+    print('Part 2:', count)
+
+
+solve_p2()
