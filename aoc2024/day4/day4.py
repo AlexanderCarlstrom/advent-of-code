2c15fe9b72d30ed2b1989ed9ec1e9793f6e50aa1
Alexander Carlström <alexander.carlstrom@pm.me>
2024-12-04 08:52:38 +0100

    2024: Completed day 4

--- aoc2024/day4/day4.py
+++ aoc2024/day4/day4.py
@@ -88,27 +88,6 @@ def solve_p2():
         chars2 = [G[p[0]][p[1]] for p in new_positions[2:]]
         if chars1.count('S') == 1 and chars1.count('M') == 1 and chars2.count('S') == 1 and chars2.count('M') == 1:
             word_count = word_count + 1
-        # diagonal1 = [G[y - 1][x - 1], G[y + 1][x + 1]].sort()
-        # diagonal2 = [G[y + 1][x - 1], G[y - 1][x + 1]].sort()
-
-        # if diagonal1 == diagonal2:
-        #     x_mas.add((y, x))
-
-    # for pos in a_positions:
-    #     y, x = pos
-    #     directions = [(y, x, 'N', 1), (y, x, 'NE', 1), (y, x, 'E', 1), (y, x, 'SE', 1), (y, x, 'S', 1), (y, x, 'SW', 1), (y, x, 'W', 1), (y, x, 'NW', 1)]
-
-    #     while len(directions) > 0:
-    #         y2, x2, dir, count = directions.pop(0)
-    #         y3, x3 = get_dir_pos(y2, x2, dir)
-    #         if not is_inside(y3, x3, G):
-    #             continue
-
-    #         if G[y3][x3] == word[count]:
-    #             if count == 3:
-    #                 word_count = word_count + 1
-    #             else:
-    #                 directions.append((y3, x3, dir, count + 1))
 
     return word_count
 
